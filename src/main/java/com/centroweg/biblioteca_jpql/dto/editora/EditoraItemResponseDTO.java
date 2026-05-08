56568e97c9e5c78c0f302866fbaad184e427cd0d
jonathan_uber <jonathanuber121207@gmail.com>
2026-05-08 14:36:53 -0300

    feat: create and implement livro, editora and autor dtos

---
+++ src/main/java/com/centroweg/biblioteca_jpql/dto/editora/EditoraItemResponseDTO.java
@@ -0,0 +1,7 @@
+package com.centroweg.biblioteca_jpql.dto.editora;
+
+public record EditoraItemResponseDTO(
+    String nome
+) {
+
+}
--- src/main/java/com/centroweg/biblioteca_jpql/dto/livro/LivroResponseDTO.java
+++ src/main/java/com/centroweg/biblioteca_jpql/dto/livro/LivroResponseDTO.java
@@ -1,5 +1,20 @@
 package com.centroweg.biblioteca_jpql.dto.livro;
 
-public record LivroResponseDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
+import com.centroweg.biblioteca_jpql.dto.autor.AutorItemResponseDTO;
+import com.centroweg.biblioteca_jpql.dto.editora.EditoraItemResponseDTO;
+
+public record LivroResponseDTO(
+    Long id,
+    String titulo,
+    String isbn,
+    BigDecimal preco,
+    LocalDate dataPublicacao,
+    String categoria,
+    EditoraItemResponseDTO editora,
+    List<AutorItemResponseDTO> autores
+) {
 }
